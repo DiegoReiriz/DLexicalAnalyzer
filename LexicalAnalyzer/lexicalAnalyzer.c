06d8edb55c3149c5922c0b017300cd7b2718703d
Diego Reiriz <diegoreiriz@gmail.com>
2016-10-22 20:20:45 +0200

    Recognition of strings and comments in the lexycal analysis

--- LexicalAnalyzer/lexicalAnalyzer.c
+++ LexicalAnalyzer/lexicalAnalyzer.c
@@ -123,10 +123,106 @@ bool checkIdentifier(LexycalAnalizer *lexycalAnalizer){
 
     }
 
-    if( c == ' ' || c == '.' || c == ';' || c == '=' || c == '*' || c == '+' || c == '-' || c == '/' || c == '(' || c == ')' || c == '[' || c == ']')
+    if( c == ' ' || c == ',' || c == '.' || c == ';' || c == '=' || c == '*' || c == '+' || c == '-' || c == '/' || c == '(' || c == ')' || c == '[' || c == ']')
         result=true;
 
         iosystemReturnToken(lexycalAnalizer->ioSystem);
+
+
+    return result;
+
+}
+
+bool checkLiteralString(LexycalAnalizer *lexycalAnalizer){
+
+    bool result =false;
+    char c=iosystemNextToken(lexycalAnalizer->ioSystem);
+
+    while( c != '"' && c != '\n'){
+        if ( c == '\\')
+            c=iosystemNextToken(lexycalAnalizer->ioSystem);
+        c=iosystemNextToken(lexycalAnalizer->ioSystem);
+    }
+    if ( c == '"'){
+        c=iosystemNextToken(lexycalAnalizer->ioSystem);
+        result=true;
+    }else
+        printf("ERROR expected \\\":");
+
+    iosystemReturnToken(lexycalAnalizer->ioSystem);
+
+
+    return result;
+
+}
+
+int checkComment(LexycalAnalizer *lexycalAnalizer){
+
+    int result =0;
+    bool end = false;
+    char c=iosystemNextToken(lexycalAnalizer->ioSystem);
+
+    if( c == '/' ){
+
+        c=iosystemNextToken(lexycalAnalizer->ioSystem);
+        while( c != '\n')
+            c=iosystemNextToken(lexycalAnalizer->ioSystem);
+
+        iosystemReturnToken(lexycalAnalizer->ioSystem);
+        result++;
+
+    }else if ( c == '*' ){
+
+        c=iosystemNextToken(lexycalAnalizer->ioSystem);
+        if (c == '*'){
+            c=iosystemNextToken(lexycalAnalizer->ioSystem);
+            if (c != '/')
+                result++;
+            else
+                end = true;
+        }
+
+
+        c=iosystemNextToken(lexycalAnalizer->ioSystem);
+        while( !end ){
+            if( c == '*'){
+                c=iosystemNextToken(lexycalAnalizer->ioSystem);
+                if( c == '/')
+                    end = true;
+            }else{
+                c=iosystemNextToken(lexycalAnalizer->ioSystem);
+            }
+
+        }
+
+        result++;
+
+    }else if ( c == '+' ){
+
+        int deep = 1;
+        c=iosystemNextToken(lexycalAnalizer->ioSystem);
+
+        while( deep ){
+            if( c == '+'){
+                c=iosystemNextToken(lexycalAnalizer->ioSystem);
+                if( c == '/')
+                    deep--;
+            }else if( c == '/'){
+                c=iosystemNextToken(lexycalAnalizer->ioSystem);
+                if( c == '+')
+                    deep++;
+            }else{
+                c=iosystemNextToken(lexycalAnalizer->ioSystem);
+            }
+
+        }
+
+        result++;
+
+    }
+//
+//    iosystemReturnToken(lexycalAnalizer->ioSystem);
+
 
 
     return result;
@@ -231,16 +327,65 @@ int getLexema(LexycalAnalizer *lexycalAnalizer){
 
                 break;
 
-            case 3:
-                iosystemReturnToken(lexycalAnalizer->ioSystem);
-                automata++;
+            case 3: // check literal string
+
+                if(c == '"'){
+                    if(checkLiteralString(lexycalAnalizer)){ // o autómataacertou analizando o lexema actual
+                        int range = iosystemRange(*lexycalAnalizer->ioSystem);
+                        printf("Literal String: ");
+                        while(range--)
+                            printf("%c",iosystemNextTailToken(lexycalAnalizer->ioSystem));
+
+                        printf("\n");
+
+                        fin = true;
+                    }else{ //O autómata fallou identificando o lexema actual
+                        fail(lexycalAnalizer,iosystemRange(*lexycalAnalizer->ioSystem));
+                        automata++;
+                    }
+
+                }else{
+                    iosystemReturnToken(lexycalAnalizer->ioSystem);
+                    automata++;
+                }
+
                 break;
 
-            case 4:
-                iosystemReturnToken(lexycalAnalizer->ioSystem);
-                automata++;
+            case 4: // detect comments
+                if(c == '/'){
+                    int result = checkComment(lexycalAnalizer);
+                    if( result == 2){ // o autómata reconeceu un comentario de documentación
+                        int range = iosystemRange(*lexycalAnalizer->ioSystem);
+                        printf("FOUND A COMMENT: ");
+                        while(range--)
+                            printf("%c",iosystemNextTailToken(lexycalAnalizer->ioSystem));
+
+                        printf("\n");
+
+                        fin = true;
+                    }else if ( result == 1){
+                        int range = iosystemRange(*lexycalAnalizer->ioSystem);
+                        printf("FOUND A COMMENT: ");
+                        while(range--)
+                            printf("%c",iosystemNextTailToken(lexycalAnalizer->ioSystem));
+
+                        printf("\n");
+
+                        fin = true;
+                    }else{ //O autómata fallou identificando o lexema actual
+                        fail(lexycalAnalizer,iosystemRange(*lexycalAnalizer->ioSystem));
+                        automata++;
+                    }
+
+                }else{
+                    iosystemReturnToken(lexycalAnalizer->ioSystem);
+                    automata++;
+                }
+
                 break;
+
             default:
+                printf("TOOOOKEN: %c\n",c);
                 iosystemNextTailToken(lexycalAnalizer->ioSystem);
                 fin=true;
                 break;
